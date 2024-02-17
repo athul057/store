fef86fbf8c94dfb1bb6997e2c1324016bc95d990
athul <athuli057@gmail.com>
2024-02-17 09:20:20 +0530

    redux

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     <FormInput type="password" name="password" label="Password" />
     <SubmitBtn className="mt-4 " text="Submit" />
     <button className="btn btn-secondary mt-4 btn-block" type="button">Guest User</button>
-    <p className="text-center">Not a member yet? <Link to='/register' className="text-secondary link link-hover">Register</Link></p>
+    <p className="text-center">Not a member yet? <Link to='/register' className="text-secondary link link-hover ">Register</Link></p>
    </Form>
   </section>
  )
