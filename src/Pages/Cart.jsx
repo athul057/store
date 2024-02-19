463b9cc72f918d022321ce085d7b925fe6e14175
athul <athuli057@gmail.com>
2024-02-19 08:59:10 +0530

    temp

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -2,7 +2,7 @@ const Cart = () => {
  return <main>
   <h1 className="mt-8 text-4xl border-b pb-8">Shopping Cart</h1>
   <div className="grid gap-8 mt-6 text-2xl font-normal lg:grid-cols-12">
-   <div className="grid lg:col-start-3 col-end-7">
+   <div className="grid lg:col-span-8">
     Cart Items
    </div>
    <div>
